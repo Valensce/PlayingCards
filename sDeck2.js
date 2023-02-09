81c2f43b9669bf8a409cc51d8c0b68bdcf068f47
Valensce /3 <pradolifu@gmail.com>
2023-02-09 14:33:09 +1000

    The second most tiresome method

---
+++ sDeck2.js
@@ -0,0 +1,28 @@
+<h1>Inquisitive</h1>
+<script>
+    let deck = [
+      {'value': 1, 'suit': 'c', 'face': 'a'},
+      {'value': 2, 'suit': 'c', 'face': '2'},
+      {'value': 3, 'suit': 'c', 'face': '3'},
+      {'value': 4, 'suit': 'c', 'face': '4'},
+      {'value': 5, 'suit': 'c', 'face': '5'},
+      {'value': 6, 'suit': 'c', 'face': '6'},
+      {'value': 7, 'suit': 'c', 'face': '7'},
+      {'value': 8, 'suit': 'c', 'face': '8'},
+      {'value': 9, 'suit': 'c', 'face': '9'},
+      {'value': 10, 'suit': 'c', 'face': '10'},
+      {'value': 10, 'suit': 'c', 'face': 'j'},
+      {'value': 10, 'suit': 'c', 'face': 'q'},
+      {'value': 10, 'suit': 'c', 'face': 'k'},
+    ]
+  //console.log's must come after all dictionaries/arrays based on my experience
+  console.log(deck);
+  console.log(deck[4]);
+  
+  console.log(deck[4].suit);
+  console.log(deck[5]['value']);
+
+  console.log(deck[5]['value']+deck[7]['value']);
+  console.log(deck[5].value+deck[7].value);
+  //these lines of code do the same thing
+</script>
