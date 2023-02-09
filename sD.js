6411e774c9215cb68eccd8b52f8991fdcefc4178
Valensce /3 <pradolifu@gmail.com>
2023-02-09 14:31:41 +1000

    Create sD.js

---
+++ sD.js
@@ -0,0 +1,28 @@
+let ds = [
+      {
+        'firstname': 'Valensce',
+        'lastname': 'Positrino',
+        'age': 'unknown',
+        'gender': 'f',
+        'needsDevice': true
+      },
+      {
+        'firstname': 'Bearhoney',
+        'lastname': 'Positrino',
+        'age': 1,
+        'gender': 'm',
+        'species': 'Canis Lupus Familiaris'
+      }
+    ]
+    
+    console.log(ds[0].lastname);
+
+    let displayName0 = ds[1].firstname + ' ' + ds[1].lastname;
+    
+    console.log(displayName0);
+
+  // console.log(ds[0].species);
+
+    let displayName1 = ds[1].name + ' ' + ds[1].species;
+    
+    console.log(displayName1);
